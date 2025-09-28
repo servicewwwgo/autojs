90d945371be5483b61137b9906eddcddf107fa1e
sungo <sungo@MAX.com>
2025-09-29 05:10:36 +0800

    添加调试配置

--- wxt.config.ts
+++ wxt.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from 'wxt';
+import { resolve } from 'node:path';
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
@@ -19,5 +20,30 @@ export default defineConfig({
     host_permissions: [
       '<all_urls>'
     ]
-  }
+  },
+  dev: {
+    server: {
+      port: 3000,
+    },
+  },
+  vite: () => ({
+    build: {
+      sourcemap: true,
+    },
+  }),
+  webExt: {
+    binaries: {
+      chrome: 'C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe',
+    },
+    startUrls: ['https://www.facebook.com'],
+    // 使用 WXT 的专用配置来指定用户数据目录
+    chromiumProfile: resolve('.chrome-profile'),
+    keepProfileChanges: true,
+    chromiumArgs: [
+      '--remote-debugging-port=9222',
+      '--no-first-run',
+      '--no-default-browser-check',
+      '--disable-features=VizDisplayCompositor'
+    ],
+  },
 });
